9555fed970f8a6e49bfec6fae33018b17f9adbc5
Florentxu <72868902+Florentxu@users.noreply.github.com>
2021-01-03 20:57:10 +0100

    mapFragment + listFragment

---
+++ app/src/main/java/com/example/biblioparis/MapsFragment.java
@@ -0,0 +1,95 @@
+package com.example.biblioparis;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+//                public void onMapReady(GoogleMap googleMap) {
+//                LatLng me = new LatLng(53, 52);
+//                googleMap.addMarker(new MarkerOptions().position(me).title("Vous"));
+//                googleMap.moveCamera(CameraUpdateFactory.newLatLng(me));
+//                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(me, 12));}
+
+public class MapsFragment extends Fragment{
+    private GoogleMap mMap;
+    Double lat = 48.866667;
+    Double lng = 2.333333;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // Quand la map est chargé
+
+                LatLng me = new LatLng(lat, lng);
+                googleMap.addMarker(new MarkerOptions().position(me).title("Vous"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(me));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(me, 11));
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        }
+    }
